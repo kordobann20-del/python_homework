5f5b02e376417b5a7332b69c9b13513952e5ef5e
Ваше Ім'я <your@email.com>
2026-03-15 10:29:35 +0200

    Added task 5 with lists

---
+++ task5.py
@@ -0,0 +1,5 @@
+inventory = ["Sword", "Shield", "Potion"] 
+inventory.append("Golden Coin") 
+print(f"Full inventory: {inventory}") 
+print(f"First item: {inventory[0]}") 
+print(f"Total items: {len(inventory)}") 
