389821d923b7de8f3d9df281fe393f906764d01d
Ваше Ім'я <your@email.com>
2026-03-15 10:28:52 +0200

    Added task 4 with strings

---
+++ task4.py
@@ -0,0 +1,7 @@
+word1 = "Game" 
+word2 = "Over" 
+full_text = word1 + " " + word2 
+print(f"Text: {full_text}") 
+print(f"Length of text: {len(full_text)}") 
+status = "You collected all coins!" 
+print(status.upper()) 
