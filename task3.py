73229c065d475a785dde1cc443cdae205e66dfb2
Ваше Ім'я <your@email.com>
2026-03-15 10:28:01 +0200

    Added task 3 with math and coins

---
+++ task3.py
@@ -0,0 +1,9 @@
+a = 20 
+b = 10 
+print(f"Number A: {a}, Number B: {b}") 
+print(f"Sum: {a + b}") 
+print(f"Difference: {a - b}") 
+print(f"Multiplication: {a * b}") 
+my_coins = 50 
+bonus = 10 
+print(f"You had {my_coins} coins, added bonus {bonus}. Total: {my_coins + bonus}") 
